ed1238290d7fd46cb1f17c5a3e616ab70607999a
TheRValtr <tech.acu678@gmail.com>
2026-02-05 20:39:02 -0500

    reverse string algorithm in JS without using reverse()

---
+++ 01_arrays/reverse string(without reverse() ).js
@@ -0,0 +1,14 @@
+function reverseString(str) {
+  let reversed = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i]; 
+  }
+
+  return reversed;
+}
+
+// Example usage:
+const originalString = "hello world";
+const reversedString = reverseString(originalString);
+console.log(reversedString);
