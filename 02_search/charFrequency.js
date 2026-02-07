124ad1f11580f8565678b49d0f93174386b559fb
TheRValtr <tech.acu678@gmail.com>
2026-02-06 19:16:52 -0500

    charFrequency Algorithm in JS

---
+++ 02_search/charFrequency.js
@@ -0,0 +1,9 @@
+function countCharacters(str) {
+    const charCount = {};
+    for (let char of str) {
+        charCount[char] = (charCount[char] || 0) + 1;
+    }
+    return charCount;
+}
+
+console.log(countCharacters("abcdefgh")); 
